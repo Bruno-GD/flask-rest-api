3f753389fecbffbc289b2527f98b65c8c8e3dcf3
Bruno-GD <91556467+Bruno-GD@users.noreply.github.com>
2022-03-11 17:43:23 +0100

    :recycle: Añadido comando para iniciar la db

--- flaskr/repository/database.py
+++ flaskr/repository/database.py
@@ -1,31 +1,39 @@
-from flask import current_app, g
+from flask import g
+from .types.Item import Item
+from sqlalchemy import insert
+from sqlalchemy.orm.session import Session  # just for annotations
+import click
 
 
 class Database:
 
+    @staticmethod
+    def get_session() -> Session:
+        from dotenv import load_dotenv
+        from os import getenv
+        from sqlalchemy import create_engine
+        from sqlalchemy.ext.declarative import declarative_base
+        from sqlalchemy.orm import sessionmaker
+
+        # load .env file environment variables
+        load_dotenv()
+
+        # get SQL Server connection string from .env
+        mysql_connection_string = getenv("MYSQL_CONNECTION_STRING")
+        # create sqlalchemy engine
+        engine = create_engine(mysql_connection_string)
+        # declare Base
+        Base = declarative_base()
+        Base.metadata.create_all(engine)
+        # create a DB Session
+        Session = sessionmaker()
+        Session.configure(bind=engine)
+        return Session()
+
     @classmethod
     def get_db(cls):
         if not g.get('session', None):
-            from dotenv import load_dotenv
-            from os import getenv
-            from sqlalchemy import create_engine
-            from sqlalchemy.ext.declarative import declarative_base
-            from sqlalchemy.orm import sessionmaker
-
-            # load .env file environment variables
-            load_dotenv()
-
-            # get SQL Server connection string from .env
-            mysql_connection_string = getenv("MYSQL_CONNECTION_STRING")
-            # create sqlalchemy engine
-            engine = create_engine(mysql_connection_string)
-            # declare Base
-            g.Base = declarative_base()
-            g.Base.metadata.create_all(engine)
-            # create a DB Session
-            Session = sessionmaker()
-            Session.configure(bind=engine)
-            g.session = Session()
+            g.session = cls.get_session()
 
         return g.session
 
@@ -34,3 +42,27 @@ class Database:
         db = g.get("session", None)
         if db is not None:
             db.close()
+
+    @staticmethod
+    @click.command()
+    def init_db():
+        db = Database.get_session()
+        db.execute("DROP TABLE IF EXISTS `items`")
+        db.execute("""CREATE TABLE IF NOT EXISTS `items` (
+                       `id` INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                       `name` VARCHAR(50),
+                       `sell_in` INT(3),
+                       `quality` INT(3)
+                   )""")
+        insert_queries = [
+            Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+            Item(name="Aged Brie", sell_in=2, quality=0),
+            Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+            Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+        ]
+        db.add_all(insert_queries)
+        db.commit()
