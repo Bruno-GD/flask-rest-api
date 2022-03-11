437557d3459a820610280cb37597d1a694562b27
Bruno-GD <91556467+Bruno-GD@users.noreply.github.com>
2022-03-11 17:44:07 +0100

    :sparkles: Registrar el comando en el CLI

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,6 +6,12 @@ from flask_restful import Api
 def create_app():
     # init flaskr
     app = Flask(__name__)
+
+    # register cli commands
+    from .repository.database import Database
+    app.cli.add_command(Database.init_db, "init-db")
+
+    # init api
     api = Api(app)
 
     from .repository.database import Database
