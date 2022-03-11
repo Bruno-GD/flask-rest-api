5e4179c47fb2cd6707bccc025559730629117455
Bruno-GD <91556467+Bruno-GD@users.noreply.github.com>
2022-03-11 14:22:51 +0100

    :white_check_mark: Nuevos casos test para los nuevos 'endpoints'

--- test/test_items.py
+++ test/test_items.py
@@ -30,3 +30,21 @@ def test_item_aged_brie(client):
     assert len(data) > 0, "Debería haber minimo 1 item"
     first_item = data[0]
     assert first_item['name'] == "Aged Brie", "No es el item correcto"
+
+
+@pytest.mark.items
+def test_item_quality(client):
+    response = client.get("/items/quality/50")
+    data = loads(response.data)
+    assert len(data) > 0, "Debería haber minimo 1 item"
+    first_item = data[0]
+    assert first_item['quality'] == 50, "'quality' no es correcto"
+
+
+@pytest.mark.items
+def test_item_sell_in(client):
+    response = client.get("/items/sell_in/3")
+    data = loads(response.data)
+    assert len(data) > 0, "Debería haber minimo 1 item"
+    first_item = data[0]
+    assert first_item['sell_in'] == 3, "'sell_in' no es correcto"
