f904d95fc25b3977d7c785615d4d88fb60095bba
Bruno-GD <91556467+Bruno-GD@users.noreply.github.com>
2022-03-11 17:43:40 +0100

    :recycle: Modificado la tabla Item

--- flaskr/repository/types/Item.py
+++ flaskr/repository/types/Item.py
@@ -4,7 +4,8 @@ from sqlalchemy import Column, String, Integer
 
 class Item(Base):
     __tablename__ = "items"
-    name = Column(String(length=50), primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(length=50))
     sell_in = Column(Integer)
     quality = Column(Integer)
 
