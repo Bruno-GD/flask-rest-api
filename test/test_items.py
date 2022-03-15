ad54a724f6879492440c1eacd07ef9558bcac5dc
Bruno-GD <91556467+Bruno-GD@users.noreply.github.com>
2022-03-15 10:37:16 +0100

    :white_check_mark: Mejorado los casos test de items

--- test/test_items.py
+++ test/test_items.py
@@ -1,32 +1,12 @@
 import pytest
-from flaskr import create_app
 from json import loads
 from flask import g
 
 
-@pytest.fixture()
-def app():
-    app = create_app()
-    app.config.update({
-        "TESTING": True,
-    })
-
-    yield app
-
-
-@pytest.fixture()
-def client(app):
-    return app.test_client()
-
-
-@pytest.fixture()
-def runner(app):
-    return app.test_cli_runner()
-
-
 @pytest.mark.items
 def test_item_aged_brie(client):
-    response = client.get("/items/Aged%20Brie")
+    response = client.get("/items/name/Aged%20Brie")
+    assert response.status_code == 200, "Respuesta no esperada: %s" % response.data
     data = loads(response.data)
     assert len(data) > 0, "Debería haber minimo 1 item"
     first_item = data[0]
@@ -35,20 +15,22 @@ def test_item_aged_brie(client):
 
 @pytest.mark.items
 def test_item_quality(client):
-    response = client.get("/items/quality/50")
+    response = client.get("/items/quality/80")
+    assert response.status_code == 200, "Respuesta no esperada: %s" % response.data
     data = loads(response.data)
     assert len(data) > 0, "Debería haber minimo 1 item"
     first_item = data[0]
-    assert first_item['quality'] == 50, "'quality' no es correcto"
+    assert first_item['quality'] == 80, "'quality' no es correcto"
 
 
 @pytest.mark.items
 def test_item_sell_in(client):
-    response = client.get("/items/sell_in/3")
+    response = client.get("/items/sell_in/2")
+    assert response.status_code == 200, "Respuesta no esperada: %s" % response.data
     data = loads(response.data)
     assert len(data) > 0, "Debería haber minimo 1 item"
     first_item = data[0]
-    assert first_item['sell_in'] == 3, "'sell_in' no es correcto"
+    assert first_item['sell_in'] == 2, "'sell_in' no es correcto"
 
 
 @pytest.mark.items
@@ -57,7 +39,15 @@ def test_new_item(client):
     with app.app_context():
         response = client.post("/items", data={"name": "Test", "quality": 1, "sell_in": 1})
         assert response.status_code == 202
+
+        # obtenemos la sesion
         db = g.get('session')
+        # importamos el type Item para sqlalchemy
+        from flaskr.repository.types.Item import Item
+        # buscamos el item insertado antes en el post
+        item = db.query(Item).filter_by(name="Test").one_or_none()
+        assert item is not None, "No está el item"
+        # borramos los cambios
         db.rollback()
 
 
